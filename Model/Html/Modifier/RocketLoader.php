3f33102b706a5cc6283eaab146e5fb68c5ed0a78
Hidro Le <hidro@forixwebdesign.com>
2023-01-31 10:31:42 +0700

    M2.4: Refactor code

--- Model/Html/Modifier/RocketLoader.php
+++ Model/Html/Modifier/RocketLoader.php
@@ -92,70 +92,79 @@ class RocketLoader extends AbstractModifier
      * @param string $html
      * @return string
      */
-    protected function modifyJavascript($html)
+    protected function modifyJavascript(string $html): ?string
     {
         $ignoreParts = $this->excludeScriptTags();
         $scriptIdentify = $this->getRocketLoaderIdentify();
-        $_html = preg_replace_callback(
-            '/<script[^>]*>(?>.*?<\/script>)/is',
-            function ($script) use ($ignoreParts, $scriptIdentify) {
-                $content = $script[0];
-                $ignore = false;
-                if (strpos($script[0], 'BASE_URL') !== false) {
-                    return $script[0];
-                }
-                if (!empty($ignoreParts)) {
-                    foreach ($ignoreParts as $ignorePart) {
-                        if (strpos($content, $ignorePart) !== false) {
-                            $ignore = true;
-                            break;
-                        }
-                    }
-                }
-                if (!$ignore) {
-                    $pattern = $scriptIdentify . '-text/javascript';
-                    $_content = preg_replace_callback(
-                        '/^(?!("\'))<script[^>]+type=["\']text\/javascript["\']|<script[^>]+type=["\']application\/javascript["\']/is', //@phpcs ignore
-                        function ($scripts) use ($pattern) {
-                            $script = $scripts[0];
-                            $script = str_replace('text/javascript', $pattern, $script);
-                            $script = str_replace('application/javascript', $pattern, $script);
-                            return $script;
-                        },
-                        $content
-                    );
-                    //If preg_replace_callback has an error. revert the html
-                    if (null !== $_content) {
-                        $content = $_content;
-                    }
-                    $content = preg_replace('/^<script?[\s|\w]>/is', sprintf('<script type="%s">', $pattern), $content);
-                    if (strpos($content, 'type=') === false) {
-                        $content = $this->formatUnTypeScript($content, $pattern);
-                    }
 
-                    if (strpos($content, 'type=') === false) {
-                        //sample tag script <script data-ommit="true">
-                        // has no src=
-                        // no type=
-                        $content = $this->formatEmptyTagScript($content, $pattern);
-                    }
-                }
+        $callback = function (array $script) use ($ignoreParts, $scriptIdentify): ?string {
+            $content = $script[0];
+
+            if ($this->shouldIgnoreScript($content, $ignoreParts)) {
                 return $content;
-            },
-            $html
-        );
-        //If preg_replace_callback has an error. revert the html
+            }
+
+            $_content = $this->updateScriptTagType($content, $scriptIdentify);
+            if (null !== $_content) {
+                $content = $_content;
+            }
+            return $this->formatScriptTag($content, $scriptIdentify);
+        };
+
+        $_html = preg_replace_callback('/<script[^>]*>(?>.*?<\/script>)/is', $callback, $html);
+
         if (null !== $_html) {
-            $html = $_html;
-            //<script src="" data-cf-settings="-|49" defer=""></script>
-            $rocketLoaderTagHtml = sprintf(
+            $rocketLoaderTag = sprintf(
                 '<script src="%s" data-cf-settings="%s-|49" defer=""></script>',
                 static::getRocketLoaderScript(),
                 $scriptIdentify
             );
-            $html = str_replace('</body', $rocketLoaderTagHtml . '</body', $html);
+            $html = str_replace('</body', $rocketLoaderTag . '</body', $_html);
         }
+
         return $html;
+    }
+
+    private function shouldIgnoreScript(string $content, array $ignoreParts): bool
+    {
+        if (strpos($content, 'BASE_URL') !== false) {
+            return true;
+        }
+
+        foreach ($ignoreParts as $ignorePart) {
+            if (strpos($content, $ignorePart) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function updateScriptTagType(string $content, string $scriptIdentify): ?string
+    {
+        $pattern = $scriptIdentify . '-text/javascript';
+        $callback = function (array $scripts) use ($pattern): ?string {
+            $script = $scripts[0];
+            $script = str_replace('text/javascript', $pattern, $script);
+            $script = str_replace('application/javascript', $pattern, $script);
+            return $script;
+        };
+
+        return preg_replace_callback('/^<script[^>]+type=["\']text\/javascript["\']|<script[^>]+type=["\']application\/javascript["\']/is', $callback, $content);
+    }
+
+    private function formatScriptTag(string $content, string $scriptIdentify): ?string
+    {
+        $pattern = $scriptIdentify . '-text/javascript';
+        $content = preg_replace('/^<script?[\s|\w]>/is', sprintf('<script type="%s">', $pattern), $content);
+        if (strpos($content, 'type=') === false) {
+            $content = $this->formatUnTypeScript($content, $pattern);
+        }
+        if (strpos($content, 'type=') === false) {
+            $content = $this->formatEmptyTagScript($content, $pattern);
+        }
+
+        return $content;
     }
 
     /**
